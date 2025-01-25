f86abbd1bc6f2ba5a798e4a7be66b12272460754
Thiago <thiagofelipefreire@gmail.com>
2025-01-25 10:19:40 -0300

    Correção no estilo do dropdown (select e option)

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Servindo os arquivos estáticos
 app.use(express.static(path.join(__dirname, '')));
 
-// Rota para o arquivo HTML
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
